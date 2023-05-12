d8c8925cc0dfe229c193e4e1a56284654405edd4
Bryan Armando <mamanibryan1@gmail.com>
2023-05-11 20:59:51 -0500

    add title and format number

--- components/Property.jsx
+++ components/Property.jsx
@@ -5,20 +5,21 @@ import { Avatar } from '@chakra-ui/avatar'
 import { FaBed, FaBath } from 'react-icons/fa'
 import { BsGridFill } from 'react-icons/bs'
 import { GoVerified } from 'react-icons/go'
+import { formatNumber } from '@/utils/currencyFormat'
 
 import DefaultImage from '@/assets/images/house.jpg'
 
 const Property = ({ coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID }) => (
-  <Link href={`/property/${externalID}`} passHref>
+  <Link href={`/property/${externalID}`} passHref style={{ flexGrow: 1 }}>
     <Flex flexWrap='wrap' w='420px' p='5' paddingTop='0px' justifyContent='flex-start' cursor='pointer'>
       <Box>
-        <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt={title.substring(0, 30)} />
+        <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} style={{ aspectRatio: '16/9' }} alt={title.substring(0, 30)} />
       </Box>
       <Box w='full'>
         <Flex paddingTop='2' alignItems='center' justifyContent='space-between'>
           <Flex alignItems='center'>
             <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified />}</Box>
-            <Text fontWeight='bold' fontSize='lg'>AED {price}{rentFrequency && `/${rentFrequency}`}</Text>
+            <Text fontWeight='bold' fontSize='lg'>AED {formatNumber.format(price)}{rentFrequency && `/${rentFrequency}`}</Text>
           </Flex>
           <Box>
             <Avatar size='sm' src={agency?.logo?.url} />
@@ -26,7 +27,7 @@ const Property = ({ coverPhoto, price, rentFrequency, rooms, title, baths, area,
         </Flex>
         <Flex alignItems='center' p='1' justifyContent='space-between' w='250px' color='blue.400'>
           {rooms}
-          <FaBed /> | {baths} <FaBath /> | {area} sqft <BsGridFill />
+          <FaBed /> | {baths} <FaBath /> | {formatNumber.format(area)} sqft <BsGridFill />
         </Flex>
         <Text fontSize='lg'>
           {title.length > 30 ? title.substring(0, 30) + '...' : title}
